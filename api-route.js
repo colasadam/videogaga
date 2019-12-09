377ebc9d3b4a281ba611270f3d9dfa2eadeb06d4
adamcolas <colas.adam@gmail.com>
2019-12-09 15:10:51 +0100

    mise en place dossier

--- api-route.js
+++ api-route.js
@@ -3,11 +3,59 @@ var express = require('express');
 var app = require('express').Router();;
 var bodyParser = require('body-parser');
 
+const Streamlink = require('streamlink');
+
 var cors = require('cors');
 
-app.get('/', function(req, res) {
+var ytdl = require('ytdl-core');
+
+app.use(cors());
+
+app.get('/', function (req, res) {
     console.log("home");
-    res.sendFile(__dirname + '/home.html');;
+    res.sendFile(__dirname + '/home.html');
+
+
+    ytdl.getInfo("M9ak5x-NYxc", (err, info) => {
+        if (err) throw err;
+        let format = ytdl.chooseFormat(info.formats, { quality: '134' });
+        if (format) {
+            console.log(format.url);
+        }
+    });
+
+    /*var link = "https://www.youtube.com/watch?v=M9ak5x-NYxc";
+    var stream = new Streamlink(link).output('./' + Date.now() + '.flv').start();
+
+    stream.getQualities();
+
+    stream.on('quality', (data) => {
+        console.log(data);
+    });
+    
+    stream.on('err', (err) => {
+        console.log(err);
+    });
+    
+    stream.on('end', (o) => {
+        console.log("Stream ended");
+        console.log(o);
+    });
+    
+    stream.on('log', (data) => {
+        console.log(data);
+    });*/
+});
+
+
+app.get('/watch/:videoId', function (req, res) {
+    ytdl.getInfo(req.params.videoId, (err, info) => {
+        if (err) throw err;
+        let format = ytdl.chooseFormat(info.formats, { quality: '134' });
+        res.json(format.url);
+    })
 });
 
 module.exports = app
+
+
