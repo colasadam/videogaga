c7039c037d1d0fe0ed3275f22f209d0d42361faa
adamcolas <colas.adam@gmail.com>
2019-12-17 16:20:44 +0100

    inscription

--- login.js
+++ login.js
@@ -19,4 +19,24 @@ appConnexion.controller('mainController', ['$scope','$http','$cookies',function(
                 }
             })
     }
+
+    $scope.inscription = function(){
+        user = document.getElementById("login").value
+        password = document.getElementById("password").value
+        password2 = document.getElementById("password2").value
+        if(password2==password){
+            $http.post("http://localhost:8083/add_user/"+user+"/"+password)
+            .then(function(cb){
+                if (cb.data !=null){
+                    $cookies.put('user', user);
+                    //$cookies.user = user
+                    document.location.href = "/"
+                } 
+            })
+        }
+        else{
+            window.alert("Mots de passe différents")
+        }
+
+    }
 }]);
